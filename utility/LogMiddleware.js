cc8f5c3aa4fbf0afc69171883e6ddec5f9caf3d6
xenedium <aabderraziq@gmail.com>
2022-02-25 21:10:40 +0100

    error handling

--- utility/LogMiddleware.js
+++ utility/LogMiddleware.js
@@ -19,8 +19,13 @@ export default function LogMiddleware(req, res, next) {         //TODO
             })}\n\`\`\``
         })
     })
+    try{
+        db.query("INSERT INTO logs (ip, method, url, user_agent, referer, timestamp) VALUES (?, ?, ?, ?, ?, ?)", [req.ip.split(':')[3], req.method, req.url, req.headers["user-agent"], req.headers["referer"], Math.floor(new Date().getTime() / 1000)])
+    }
+    catch(e){
+        console.log(e)
+    }
     
-    db.query("INSERT INTO logs (ip, method, url, user_agent, referer, timestamp) VALUES (?, ?, ?, ?, ?, ?)", [req.ip.split(':')[3], req.method, req.url, req.headers["user-agent"], req.headers["referer"], Math.floor(new Date().getTime() / 1000)])
     
     next();
 }
