70bc0cf39192d135196d5668b9950dce3b14d643
xenedium <aabderraziq@gmail.com>
2022-02-25 01:01:34 +0100

    ipv4 log fix

--- utility/LogMiddleware.js
+++ utility/LogMiddleware.js
@@ -3,7 +3,7 @@ const hookurl = require("../config.json").discordhook;
 import db from "../utility/Database";
 
 export default function LogMiddleware(req, res, next) {         //TODO
-    /*
+    
     fetch(hookurl, {
         method: "POST",
         headers: {
@@ -12,7 +12,7 @@ export default function LogMiddleware(req, res, next) {         //TODO
         body: JSON.stringify({
             username: "LogMiddleware",
             content: `\`\`\`json\n${JSON.stringify({
-                ip: req.ip,
+                ip: req.ip.split(':')[3],
                 method: req.method,
                 url: req.url,
                 headers: req.headers,
@@ -20,11 +20,7 @@ export default function LogMiddleware(req, res, next) {         //TODO
         })
     })
     
-    db.query("INSERT INTO logs (ip, method, url, user_agent, referer, timestamp) VALUES (?, ?, ?, ?, ?, ?)", 
-        [req.ip, req.method, req.url, req.headers["user-agent"], req.headers["referer"], Math.floor(new Date().getTime() / 1000)], 
-        (err, result) => {
-            if (err) res.status(500).json({error: "Internal Server Error"});
-    })
-    */
+    db.query("INSERT INTO logs (ip, method, url, user_agent, referer, timestamp) VALUES (?, ?, ?, ?, ?, ?)", [req.ip.split(':')[3], req.method, req.url, req.headers["user-agent"], req.headers["referer"], Math.floor(new Date().getTime() / 1000)])
+    
     next();
 }
