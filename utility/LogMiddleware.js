c919aee11f5ebf1340f9c6557f97cb8da6db645c
xenedium <aabderraziq@gmail.com>
2022-02-26 11:41:29 +0100

    Exception handling with callback

--- utility/LogMiddleware.js
+++ utility/LogMiddleware.js
@@ -3,7 +3,7 @@ const hookurl = require("../config.json").discordhook;
 import db from "../utility/Database";
 
 export default function LogMiddleware(req, res, next) {         //TODO
-    
+
     fetch(hookurl, {
         method: "POST",
         headers: {
@@ -19,13 +19,15 @@ export default function LogMiddleware(req, res, next) {         //TODO
             })}\n\`\`\``
         })
     })
-    try{
-        db.query("INSERT INTO logs (ip, method, url, user_agent, referer, timestamp) VALUES (?, ?, ?, ?, ?, ?)", [req.ip.split(':')[3], req.method, req.url, req.headers["user-agent"], req.headers["referer"], Math.floor(new Date().getTime() / 1000)])
-    }
-    catch(e){
-        console.log(e)
-    }
-    
-    
+    db.query("INSERT INTO logs (ip, method, url, user_agent, referer, timestamp) VALUES (?, ?, ?, ?, ?, ?)", [req.ip.split(':')[3], req.method, req.url, req.headers["user-agent"], req.headers["referer"], Math.floor(new Date().getTime() / 1000)],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+        })
+
+
+
+
     next();
 }
