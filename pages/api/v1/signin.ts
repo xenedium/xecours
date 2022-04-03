77253df5ded6920fa29374d15f928c20053b10f7
xenedium <aabderraziq@gmail.com>
2022-04-03 15:16:46 +0000

    megalinter & signin

--- pages/api/v1/signin.ts
+++ pages/api/v1/signin.ts
@@ -1,13 +1,60 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { PrismaClient } from '@prisma/client'
+import { sign } from 'jsonwebtoken'
+import { createHash } from 'crypto'
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const prisma = new PrismaClient();
-    
-    //console.log(req.body);
+    const secret = process.env.SECRET;
 
-    res.status(501).json({
-        error: "Not implemented"
-    });
+    res.setHeader('Content-Type', 'application/json');
+
+    if (req.method !== 'POST') {
+        res.status(405).send({
+            error: 'Method not allowed'
+        });
+        return;
+    }
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        res.status(400).json({
+            error: 'Please provide email and password'
+        });
+        return;
+    }
+
+    try
+    {
+        const user = await prisma.users.findFirst({
+            where: {
+                username: username,
+                password: createHash('sha256').update(password).digest('hex')
+            }
+        });
+
+        if (!user) {
+            res.status(401).json({
+                error: 'Invalid credentials'
+            });
+            return;
+        }
+
+        const token = sign({username: user.username}, secret, {expiresIn: '72h'});
+        res.status(200).json({
+            token: token
+        });
+
+    }
+    catch (error)
+    {
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+        return;
+    }
+
+
 }
